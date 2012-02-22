b2fbd31b453dc4c9b0878cea079bdad507dc200b
StephenL <stephen.lu@utah.edu>
2012-02-22 13:27:40 -0700

    Added some skeleton structure to the Metadata verifier.  This project will be used for checking single pieces of metdata and compare against directorys.  SL

---
+++ MetaData_Verifier/MetaData_Verifier/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace MetaData_Verifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StoreInfoVerifier(args[0]);
+        }
+
+        static XElement LoadMetaData(FileInfo data)
+        {
+
+            // Element will get overwritten
+            XElement metadata = new XElement(data.FullName);
+            if (!isMetaData(data))
+            {
+                FileInfo newData = new FileInfo(Console.ReadLine());
+                LoadMetaData(newData);
+            }
+            else
+            {
+                metadata = XElement.Load(data.FullName);
+            }
+
+            return metadata;
+
+        }
+
+        static bool isMetaData(FileInfo data)
+        {
+            if (!data.Exists || data.Extension != ".dsx")
+            {
+                MessageBox.Show("Error:  This is not a MetaData File.  MetaData must have a .dsx extension");
+                return false;
+            }
+            return true;
+        }
+
+        static void StoreInfoVerifier(XElement metadata)
+        {
+            // Check Product Value
+
+            // Check StoreID
+
+            // Check GlobalID
+ 
+            // 
+
+
+            
+        }
+    }
+}
